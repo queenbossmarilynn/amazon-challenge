febfbb32a24d551750395f0c6aaea07ccb17e7d5
queenbossmarilynn <marilynnmelissaramirez@gmail.com>
2021-12-20 23:38:25 -0800

    Bug for image has been solved. Changed to another link. Trying to get my icons to appear next.

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
     return (
         <div className='header'>
             <img 
                 className = "header__logo" 
-                src = "./media/white-amazon-logo.png/"
+                src = "http://pngimg.com/uploads/amazon/amazon_PNG11.png"
             />
             
             <div className="header__search">
                 <input className="header__searchInput"
                     type="text" />
+                    <SearchIcon className="header__searchIcon"/>
                     {/*Logo*/}
             </div>
             <div className="header_nav">
